56d3f9ea486fcbffdfae16a289079e11814ccaff
范云飞 <34054835+NiceYouKnow@users.noreply.github.com>
2019-09-28 11:35:15 +0800

    Update views.py

--- app/main/views.py
+++ app/main/views.py
@@ -152,7 +152,7 @@ def edit(id):
     return render_template('edit_post.html', form=form)
 
 
-@main.route('/delete/<int:id>', methods=['POST'])
+@main.route('/delete/<int:id>', methods=['GET', 'POST'])
 @login_required
 def delete(id):
     post = Post.query.get_or_404(id)
